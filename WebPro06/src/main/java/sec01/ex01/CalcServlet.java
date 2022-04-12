69efa90c3f90bc659e24cda26ec9675e4a21e6ef
ユンヨンヘ <102148190+futureDevvvv@users.noreply.github.com>
2022-04-12 20:41:22 +0900

    Add files via upload

---
+++ WebPro06/src/main/java/sec01/ex01/CalcServlet.java
@@ -0,0 +1,69 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalcServlet
+ */
+@WebServlet("/Calc")
+public class CalcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		String command = request.getParameter("command");
+		String won = request.getParameter("won");
+		String operator = request.getParameter("operator");
+		
+		if(command != null && command.equals("calculate"))
+		{
+			String result = calculate(Float.parseFloat(won), operator);
+			pw.print("<html>" + result);
+			pw.print("<a href='/pro06/calc'>환율계산기</a>");
+			return;
+		}
+	
+	}
+
+	private String calculate(float parseFloat, String operator) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
